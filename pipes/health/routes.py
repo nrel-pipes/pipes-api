ed6c3fb780a50b32fb578fa7ae3fbf225a901b58
jgu2 <jianli.gu@nrel.gov>
2024-03-08 11:22:56 -0700

    Update health

--- pipes/health/routes.py
+++ pipes/health/routes.py
@@ -1,20 +1,41 @@
 from __future__ import annotations
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status
+
+from pipes.db.neptune import NeptuneDB
 
 router = APIRouter()
 
 
+def get_neptune_db():
+    """A dependency function for FastAPI"""
+    db = NeptuneDB()
+    try:
+        db.connect()
+        yield db
+    finally:
+        db.close()
+
+
 @router.get("/")
 async def welcome():
-    return {"message": "Hello PIPES!"}
+    return {"message": "Welcome to NREL PIPES!"}
 
 
 @router.get("/ping/")
-async def ping():
+async def ping(neptune: NeptuneDB = Depends(get_neptune_db)):
     """
     For ALB health check, need to ping Neptune regularly to avoid idle timeout - 20mins
 
     https://docs.aws.amazon.com/neptune/latest/userguide/limits.html#limits-websockets
     """
-    return {"message": "pong"}
+    try:
+        data = neptune.ping()
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(e),
+        )
+
+    vertex = data[0] if len(data) > 0 else None
+    return {"message": "pong", "vertex": vertex.label}
