799789ec9801b72eeefb67fc799377d4684b84e4
jgu2 <jianli.gu@nrel.gov>
2024-03-31 11:36:58 -0600

    Add constants

--- pipes/common/constants.py
+++ pipes/common/constants.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 DNS_ORG_MAPPING = {
     "lbl.gov": "Lawrence Berkeley National Laboratory (LBNL)",
     "ornl.gov": "Oak Ridge National Laboratory (ORNL)",
@@ -17,3 +19,19 @@ DNS_ORG_MAPPING = {
     "netl.doe.gov": "National Energy Technology Laboratory (NETL)",
     "inl.gov": "Idaho National Laboratory (INL)",
 }
+
+
+class VersionStatus(str, Enum):
+    Active = "Active"
+    Inactivate = "Inactivate"
+    Unresolved = "Unresolved"
+
+
+class VertexLabel(str, Enum):
+    User = "User"
+    Team = "Team"
+    Project = "Project"
+    ProjectRun = "ProjectRun"
+    Model = "Model"
+    ModelRun = "ModelRun"
+    Task = "Task"
