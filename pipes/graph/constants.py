b594200b20211e13f8fc7c1a747d5c53f3f9eac0
jgu2 <jianli.gu@nrel.gov>
2024-04-15 13:44:16 -0600

    Update graph edge enum

--- pipes/graph/constants.py
+++ pipes/graph/constants.py
@@ -23,3 +23,9 @@ class EdgeLabel(str, Enum):
     produced = "produced"  # Model run produces dataset
     feeds = "feeds"  # Model A feeds model B
     informs = "informs"  # Model B informs model A
+    associated = "associated"  # Task is associated with user
+    used = "used"  # Task used dataset as input
+    output = "output"  # Task output dataset
+    connected = "connected"  # Model run connected to dataset output from task
+    consumed = "consumed"  # Model run consumed dataset
+    relates = "relates"  # task relates to another task
